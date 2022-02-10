217d4c0feed9f8761023996bcf474a57e655b676
= <chhatanidarvesh8@gmail.com>
2022-02-10 10:35:59 +0530

    Laravel Company Project

--- company/app/Services/CreateUserService.php
+++ company/app/Services/CreateUserService.php
@@ -8,11 +8,7 @@ use Hash;
 class CreateUserService{
 
     public function store(array $userParams,Company $company) :User {     
-         /* if($request->has('password')){
-                $request->merge(['password' => Hash::make($request->password)]);
-          }*/
-             $User = $company->users()->create($userParams);
-
+           $User = $company->users()->create($userParams);
              return $User;
       }
 }
